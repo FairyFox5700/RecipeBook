66ac07b24f4159e1be7c4c5c83b65268885ba884
Ірина Тищенко <tyschenk20@gmail.com>
2020-01-24 11:19:34 +0200

    Created new datatabase and added init data

---
+++ RecipeBook/DataAccess/CookDbContext.cs
@@ -0,0 +1,25 @@
+﻿using RecipeBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeBook.DataAccess
+{
+    public class CookDbContext:DbContext
+    {
+        public CookDbContext() : base("CookBookDbConnection")
+        {
+            //Database.SetInitializer<CookDbContext>(new CookDbInitializator());
+
+        }
+
+        public virtual DbSet<Recipe> Recipes { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Ingredient> Ingredients { get; set; }
+        public virtual DbSet<Nutritious> Nutritious { get; set; }
+        public virtual DbSet<RecipeImage> RecipeImages { get; set; }
+    }
+}
--- RecipeBook/MainWindow.xaml.cs
+++ RecipeBook/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RecipeBook.DataAccess;
+using RecipeBook.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,7 @@ namespace RecipeBook
         public MainWindow()
         {
             InitializeComponent();
+            
         }
     }
 }
